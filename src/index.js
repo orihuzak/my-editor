ad5e2de6b29ee443bcceaa15001e228ebc2fbc41
原口和大 <arx.solid@gmail.com>
2018-11-28 04:05:22 +0900

    UI開発の雛形を作成

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import $ from 'jquery'
+
+import MyEditor from './components/myeditor'
+
+
+class Line extends Node {
+  constructor() {
+    super()
+    this.line = document.createElement('div')
+    this.line.className = 'line'
+    this.line.contentEditable = 'true'
+    this.line.oninput = this.getLine
+  }
+
+  getLine() {
+    return console.log(this.line.innerHTML)
+  }
+
+  inner() {
+    return this.line.innerHTML
+  }
+
+}
+
+function createPage() {
+  const page = document.createElement('div')
+  page.className = 'page'
+  page.contentEditable = 'true'
+  page.oninput = () => { console.log(page.innerHTML) }
+  return page
+}
+
+function main() {
+  const body = document.body
+  body.appendChild(createPage())
+  body.onclick = () => (console.log(window.getSelection()))
+}
+
+main()
+
+ReactDOM.render(
+  <MyEditor />,
+  document.getElementById('example')
+)
+
+module.hot.accept()
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    hot: true,
+    port: 9000
+  }
+}
